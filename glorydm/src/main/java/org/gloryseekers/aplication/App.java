fcc4229fb0c954682957cba985bc03ddbb3a30c1
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2021-09-30 18:47:28 +0200

    .

--- glorydm/src/main/java/org/gloryseekers/aplication/App.java
+++ glorydm/src/main/java/org/gloryseekers/aplication/App.java
@@ -6,7 +6,5 @@ import org.gloryseekers.infra.JavaFXPainter;
 public class App {
     public static void main(String[] args) {
         CharacterManager characterManager = new CharacterManager(new JavaFXPainter(), new CharacterXML()); // maybe this can be loaded by spring later
-
-        
     }
 }
