f9743a375a56a6862dd3883838dbca42bbb62414
Marco <markolas14@gmail.com>
2021-11-14 14:59:14 +0100

    Hotfix

--- glorydm/src/main/java/module-info.java
+++ glorydm/src/main/java/module-info.java
@@ -5,7 +5,7 @@ module org.gloryseekers {
     requires javafx.fxml;
     //requires com.ctc.wstx; //Woodstox StAX
     exports org.gloryseekers;
-    exports org.gloryseekers.infra to javafx.graphics;
+    exports org.gloryseekers.infra.persistance.characters to javafx.graphics;
     exports org.gloryseekers.domain.model to com.fasterxml.jackson.databind;
     exports org.gloryseekers.infra.view to javafx.graphics, javafx.fxml;
     exports org.gloryseekers.infra.material to javafx.fxml;
--- glorydm/src/main/java/org/gloryseekers/App.java
+++ glorydm/src/main/java/org/gloryseekers/App.java
@@ -2,15 +2,14 @@ package org.gloryseekers;
 
 import org.gloryseekers.aplication.CharacterManager;
 import org.gloryseekers.domain.*;
-import org.gloryseekers.infra.*;
 import org.gloryseekers.infra.view.UI;
-
+import org.gloryseekers.infra.persistance.characters.CharacterPersistor;
 import org.gloryseekers.domain.model.Character;
 import org.gloryseekers.domain.model.*;
 
 public class App {
     public static void main(String[] args) {
-    	final CharacterPort storageManager = new CharacterXML(); //load storage adapter
+    	final CharacterPort storageManager = new CharacterPersistor(); //load storage adapter
         final ManagementPort managementPort = CharacterManager.getInstance(storageManager);
         final UI ui = new UI(); //load graphical interface    
         ui.start(); //launch app
