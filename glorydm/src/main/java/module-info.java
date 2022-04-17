f6a6c6f35e759849c525ce0973192d5dd3038309
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2022-04-17 17:52:03 +0000

    JLink now works as it should

--- glorydm/src/main/java/module-info.java
+++ glorydm/src/main/java/module-info.java
@@ -1,16 +1,16 @@
-module org.gloryseekers {
+module glory {
     requires javafx.controls;
     //requires com.fasterxml.jackson.dataformat.xml;
     requires com.fasterxml.jackson.databind;
     requires javafx.fxml;
-    requires org.apache.log4j;
+    requires org.apache.logging.log4j;
     //requires com.ctc.wstx; //Woodstox StAX
     exports org.gloryseekers;
     exports org.gloryseekers.infra.persistance.characters to javafx.graphics;
     exports org.gloryseekers.domain.model to com.fasterxml.jackson.databind;
     exports org.gloryseekers.infra.view to javafx.graphics, javafx.fxml;
     exports org.gloryseekers.infra.material to javafx.fxml;
-    opens org.gloryseekers to org.apache.log4j, javafx.fxml;
+    opens org.gloryseekers to org.apache.logging.log4j, javafx.fxml;
     opens org.gloryseekers.infra.material to javafx.fxml;
     opens org.gloryseekers.infra.view.main to javafx.fxml;
     opens org.gloryseekers.infra.view to javafx.fxml;
--- glorydm/src/main/java/org/gloryseekers/infra/log/GSLogger.java
+++ glorydm/src/main/java/org/gloryseekers/infra/log/GSLogger.java
@@ -2,8 +2,8 @@ package org.gloryseekers.infra.log;
 
 
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.gloryseekers.domain.model.LogType;
 
 public class GSLogger {
